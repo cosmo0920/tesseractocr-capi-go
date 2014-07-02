f6098affad23ad3e3ba99ae47e7ff04c2b990878
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-02 10:24:16 +0900

    add install.sh for Travis
    Becasu Travis Ubuntu Linux image is too old for CI.

---
+++ install.sh
@@ -0,0 +1,33 @@
+sudo apt-get install zlib1g-dev
+sudo apt-get install libpng12-dev libjpeg62-dev libtiff4-dev libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl1.2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev
+sudo apt-get install python-enchant python-poppler
+
+cd /tmp
+wget http://www.leptonica.com/source/leptonica-1.69.tar.gz
+tar -xvzof leptonica-1.69.tar.gz
+cd leptonica-1.69
+./configure
+make
+sudo make install
+sudo ldconfig
+
+cd /tmp
+wget http://tesseract-ocr.googlecode.com/files/tesseract-3.02.02.tar.gz
+tar -xvzof tesseract-3.02.02.tar.gz
+cd tesseract-3.02.02
+automake
+./configure
+sudo rm -f /usr/local/share/tessdata
+sudo mkdir /usr/local/share/tessdata
+make
+sudo make install
+sudo ldconfig
+
+cd /tmp
+wget http://tesseract-ocr.googlecode.com/files/eng.traineddata.gz
+wget http://tesseract-ocr.googlecode.com/files/jpn.traineddata.gz
+cd /usr/local/share/tessdata
+gzip -d /tmp/eng.traineddata.gz
+gzip -d /tmp/jpn.traineddata.gz
+sudo cp /tmp/eng.traineddata /usr/local/share/tessdata
+sudo cp /tmp/jpn.traineddata /usr/local/share/tessdata
