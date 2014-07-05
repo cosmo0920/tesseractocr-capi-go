f9a0622ac91cd9642dddfb15703e49f0bf9f6921
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-06 04:07:32 +0900

    add `TesseBaseAPIInit Test and refactoring

--- tesseractocr_test.go
+++ tesseractocr_test.go
@@ -8,20 +8,30 @@ import (
 func TestBaseAPINew(t *testing.T) {
 	result := ocr.BaseAPINew()
 	if result == nil {
-		t.Errorf("result = %v cannot nil", result, nil)
+		t.Errorf("result = %v cannot nil", result)
+	}
+}
+
+func TestBaseAPIInit(t *testing.T) {
+	lang := "eng"
+	env := ocr.Env()
+	api := ocr.BaseAPINew()
+	_, err := api.BaseAPIInit3(env, lang)
+	if err != nil {
+		t.Errorf("err = %v initialize failure", err)
 	}
 }
 
 func TestVersion(t *testing.T) {
 	result := ocr.Version()
 	if result == "" {
-		t.Errorf("result = %v cannot empty string", result, nil)
+		t.Errorf("result = %v cannot empty string", result)
 	}
 }
 
 func TestEnv(t *testing.T) {
 	result := ocr.Env()
 	if result == "" {
-		t.Errorf("result = %v cannot empty string", result, nil)
+		t.Errorf("result = %v cannot empty string", result)
 	}
 }
