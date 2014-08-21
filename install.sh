04e9b63e26d7bc99c6b96a23ba7ee61e3840554b
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-21 23:18:26 +0900

    travis: use multi core if available

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 sudo apt-get install zlib1g-dev
 sudo apt-get install libpng12-dev libjpeg62-dev libtiff4-dev libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl1.2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev
 sudo apt-get install python-enchant python-poppler
+n_processors="$(grep '^processor' /proc/cpuinfo | wc -l)"
 
 cd /tmp
 wget http://www.leptonica.com/source/leptonica-1.70.tar.gz
 tar -xvzof leptonica-1.70.tar.gz
 cd leptonica-1.70
 ./configure
-make
+make -j${n_processors}
 sudo make install
 sudo ldconfig
 
@@ -19,7 +20,7 @@ automake
 ./configure
 sudo rm -f /usr/local/share/tessdata
 sudo mkdir /usr/local/share/tessdata
-make
+make -j${n_processors}
 sudo make install
 sudo ldconfig
 
